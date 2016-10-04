58534cf9dbbe1d24460eba89db458c9a24e9dc23
dapeng126 <zhangdapeng89@126.com>
2016-10-04 22:50:16 +0800

    添加 jdbc

---
+++ src/com/jdbc/dbutils/JDBCUtils.java
@@ -0,0 +1,22 @@
+package com.jdbc.dbutils;
+
+import com.mysql.jdbc.Driver;
+
+
+/**
+ * Created by durban126 on 16/10/4.
+ */
+public class JDBCUtils {
+    private final String USERNAME = "root";
+    private final String PASS = "123456";
+    private final String DRIVER = "com.mysql.jdbc.Driver";
+    private final String URL = "jdbc:mysql://127.0.0.1:3306/test";
+
+    public JDBCUtils(){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}

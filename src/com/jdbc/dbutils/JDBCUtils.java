69bad7ea4354b9f8b08c5b4168741967411b5d8c
dapeng126 <zhangdapeng89@126.com>
2016-10-04 23:06:36 +0800

    添加 JDBCUtils

--- src/com/jdbc/dbutils/JDBCUtils.java
+++ src/com/jdbc/dbutils/JDBCUtils.java
@@ -1,6 +1,13 @@
 package com.jdbc.dbutils;
 
-import com.mysql.jdbc.Driver;
+import java.sql.PreparedStatement;
+
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 
 /**
@@ -11,12 +18,56 @@ public class JDBCUtils {
     private final String PASS = "123456";
     private final String DRIVER = "com.mysql.jdbc.Driver";
     private final String URL = "jdbc:mysql://127.0.0.1:3306/test";
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+    private ResultSet resultSet;
 
-    public JDBCUtils(){
+    public JDBCUtils() {
+        try {
+            Class.forName(DRIVER);
+            System.out.println("注册驱动成功！");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //定义数据库的链接
+    public Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASS);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    /**
+     * UPDATE
+     * @param sql
+     * @param params
+     * @return
+     * @throws SQLException
+     */
+    public boolean updateByPreparedStatement(String sql, List<Object> params) throws SQLException {
+        boolean flag = false;
+
+        int result = 1;
+        preparedStatement = connection.prepareStatement(sql);
+        int index = 1;
+        if (params != null && !params.isEmpty()) {
+            for (int i = 0; i < params.size(); i++) {
+                preparedStatement.setObject(index++, params.get(i));
+            }
+        }
+
+        result = preparedStatement.executeUpdate();
+        flag = result > 0 ? true : false;
+        return flag;
 
     }
 
-    public static void main(String[] args){
 
+    public static void main(String[] args) {
+        JDBCUtils jdbcUtils = new JDBCUtils();
     }
 }
